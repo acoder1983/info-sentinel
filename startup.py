d9c0f87a5d88b6da8d07244d212db0f2a2074cec
c00344060 <chengqi3@huawei.com>
2017-12-12 20:08:34 +0800

    daemon

--- startup.py
+++ startup.py
@@ -6,19 +6,52 @@ from random import random
 
 from bitcoin import BitCoins
 from stock import Stocks
+import multiprocessing
+import sys
 
 
-def main():
-    targets = BitCoins#+Stocks
+def main(args):
+    if len(args) > 1:
+        run_as_daemon()
+    else:
+        run()
+
+
+INTERVAL = 300
+
+
+def run():
+    targets = BitCoins + Stocks
     while True:
+        with open('pid', 'w') as f:
+            f.write(str(datetime.now()))
         for t in targets:
             try:
                 t.update()
             except Exception as e:
                 print(e)
             time.sleep(random())
-        time.sleep(300)
+        time.sleep(INTERVAL)
+
+
+def run_as_daemon():
+    p = multiprocessing.Process(target=run)
+    p.start()
+    time.sleep(3)
+    with open('pid') as f:
+        text = f.read()
+    while True:
+        time.sleep(INTERVAL * 3)
+        with open('pid') as f:
+            t = f.read()
+        if t != text:
+            text = t
+        else:
+            print('%s terminating daemon' % datetime.now())
+            p.terminate()
+            p = multiprocessing.Process(target=run)
+            p.start()
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
